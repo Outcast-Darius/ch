63970f17b591853b7c8b6be07df29fa80532d95b
outcastdho <outcastdarius@gmail.com>
2026-03-31 13:36:11 +0300

    styling path

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
     console.log(`Incoming Request: ${req.method} ${req.url}`);
     next();
 });
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/views', express.static(path.join(__dirname, 'views')));
 app.use('/posters', express.static(path.join(__dirname, 'posters')));
 app.use('posters', express.static(path.join(__dirname, 'posters')));
