9886fa3bf534dda8aaf1ea340caea5b3e9b84555
outcastdho <outcastdarius@gmail.com>
2026-03-31 14:35:55 +0300

    fetch request api call update

--- views/admin.js
+++ views/admin.js
@@ -1,6 +1,6 @@
 
 
-const API_URL = 'window.location.origin' || "http://127.0.0.1:5000";
+const API_URL = window.location.origin || "http://127.0.0.1:5000";
 
 async function displayAdmins() {
     try {
